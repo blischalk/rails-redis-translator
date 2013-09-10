1b84bb3f1980769e25e8a5bc91f7888e63551dae
Brett Lischalk <brett@brettlischalk.com>
2013-09-09 21:15:51 -0500

    Committing crafting rails applications rails redis translator.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Translator::Application.config.secret_key_base = 'a96cda3e940e2064068518c2e8cdd9f0369c7dbf3165bd32a8538fa4589f1c26e9eedd1ba67fcfbe6c9d9dae742628c1d633b556aa0b8a4652d37eb6c3213425'
